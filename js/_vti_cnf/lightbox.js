3b5b3fa2fa38193822e52dafcc87a5c6cb0e72c5
Marco Steinhaeuser <info@marco-steinhaeuser.de>
2025-10-18 21:15:41 +0200

    new website version with HTML5/CSS template

--- js/_vti_cnf/lightbox.js
+++
@@ -1,4 +0,0 @@
-vti_encoding:SR|utf8-nl
-vti_timelastmodified:TR|25 Jun 2008 16:10:16 -0000
-vti_extenderversion:SR|5.0.2.2623
-vti_backlinkinfo:VX|Tabelle1/filme.html Tabelle1/besetzung.html Tabelle1/ruebe.htm Tabelle1/conny.htm Tabelle1/friedhelm.htm Tabelle1/micha.htm Tabelle1/musik.html Tabelle1/kontakt.html Tabelle1/manu.htm Tabelle1/cathleen.htm Tabelle1/muddy.htm Tabelle1/Impressum.html Tabelleneu/Bilder.html Tabelleganzneu/test.htm Tabelle/home.htm Tabelle1/index.htm Tabelle1/Raupe.htm Tabelleganzneu/test2.htm
